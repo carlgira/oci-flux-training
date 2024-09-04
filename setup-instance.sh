d4df4937882cd29390ee9f12fe8868e8305e9866
Carlos Alberto Giraldo Calderón <carlgira@carloss-macbook-pro.home>
2024-09-04 11:43:30 +0200

    fixes

--- setup-instance.sh
+++ setup-instance.sh
@@ -23,7 +23,7 @@ HF_KEY=`curl -L http://169.254.169.254/opc/v1/instance/ | jq -r '.metadata."HF_K
 # ComfyUI
 su -c "git clone https://github.com/comfyanonymous/ComfyUI.git /home/$USER/ComfyUI" $USER
 su -c "git clone https://github.com/carlgira/oci-flux-training /home/$USER/oci-flux-training" $USER
-su -c "cd /home/$USER/ComfyUI && python3 -m venv venv && source venv/bin/activate && pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121 xformers && pip install -r requirements.txt" $USER
+su -c "cd /home/$USER/ComfyUI && python3 -m venv venv && source venv/bin/activate && pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121 &&  pip install -r requirements.txt" $USER
 su -c "cp /home/$USER/oci-flux-training/comfyui/start.sh /home/$USER/ComfyUI/start.sh" $USER
 su -c "cd /home/$USER/ComfyUI/custom_nodes && git clone https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git" $USER
 
@@ -33,7 +33,18 @@ su -c "cd /home/$USER/ai-toolkit && echo HF_TOKEN=$HF_KEY > .env" $USER
 su -c "cd /home/$USER/ai-toolkit && git submodule update --init --recursive && python3 -m venv venv && source venv/bin/activate && pip3 install torch && pip3 install -r requirements.txt" $USER
 su -c "cp /home/$USER/oci-flux-training/flux/* /home/$USER/ai-toolkit" $USER
 
+su -c "cd /home/$USER/ComfyUI/models/unet && wget --header="Authorization: Bearer $HF_KEY" https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors" $USER
+su -c "cd /home/$USER/ComfyUI/models/clip && wget --header="Authorization: Bearer $HF_KEY" https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors" $USER
+su -c "cd /home/$USER/ComfyUI/models/clip && wget --header="Authorization: Bearer $HF_KEY" https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors" $USER
+su -c "cd /home/$USER/ComfyUI/models/vae && wget --header="Authorization: Bearer $HF_KEY" https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors" $USER
+
+
+
+
+
 cat <<EOT >> /etc/systemd/system/comfyui.service
+
+
 [Unit]
 Description=systemd service start comfyui
 [Service]
